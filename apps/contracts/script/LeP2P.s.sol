46fdf59c5bd193cb3e4b0d630bf839b97cd67084
rplusq <rafaplusq@gmail.com>
2023-07-21 21:16:19 +0200

    deployment script

--- apps/contracts/script/LeP2P.s.sol
+++ apps/contracts/script/LeP2P.s.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "../src/LeP2P.sol";
 
 contract CounterScript is Script {
+    IERC20 public token;
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        LeP2PEscrow escrow = new LeP2PEscrow(token);
+        console2.log("LeP2PEscrow:", address(escrow));
     }
 }
