df28592e3a1e45a06fce2e693b2ab60c688ec6a2
Reynaldo Quintero <reynaldoquinterom@gmail.com>
2023-07-22 17:15:06 +0200

    contracts: Update deploy script

--- apps/contracts/script/LeP2P.s.sol
+++ apps/contracts/script/LeP2P.s.sol
@@ -5,16 +5,20 @@ import "forge-std/Script.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "../src/interfaces/IWorldId.sol";
+import "../test/mocks/USDCMock.sol";
+
 import "../src/LeP2P.sol";
 
-contract CounterScript is Script {
-    IERC20 public token;
-    IWorldId public worldId;
+contract LeP2PScript is Script {
+    IWorldId public worldId = IWorldId(0x719683F13Eeea7D84fCBa5d7d17Bf82e03E3d260);
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
-        LeP2PEscrow escrow = new LeP2PEscrow(worldId, "appId", "actionId", token);
+        vm.startBroadcast();
+        USDCMock usdc = new USDCMock();
+        LeP2PEscrow escrow = new LeP2PEscrow(worldId, "app_staging_bfd6e75388e9c05925c3cd5947ef291", "register", IERC20(address(usdc)));
+        vm.stopBroadcast();
         console2.log("LeP2PEscrow:", address(escrow));
+        console2.log("USDCMock:", address(usdc));
     }
 }
