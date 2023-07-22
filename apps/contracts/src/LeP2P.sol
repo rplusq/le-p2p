bb379ae697452986bdf20d5a6212331faff25d8b
rplusq <rafaplusq@gmail.com>
2023-07-22 14:25:27 +0200

    contracts: polygon id integration

--- apps/contracts/src/LeP2P.sol
+++ apps/contracts/src/LeP2P.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@iden3/contracts/ZKPVerifier.sol";
+import "@iden3/contracts/lib/GenesisUtils.sol";
+import "@iden3/contracts/interfaces/ICircuitValidator.sol";
 import "./helpers/ByteHasher.sol";
 import "./interfaces/IWorldId.sol";
 
-contract LeP2PEscrow is AccessControl {
+contract LeP2PEscrow is AccessControl, ZKPVerifier {
     using ByteHasher for bytes;
 
     /// @notice Thrown when attempting to reuse a nullifier
@@ -34,14 +37,17 @@ contract LeP2PEscrow is AccessControl {
     /// @dev The World ID instance that will be used for verifying proofs
 	IWorldId internal immutable _worldId;
 
-	/// @dev The contract's external nullifier hash
-	uint256 internal immutable _externalNullifier;
+	/// @dev The contract's external worldcoin nullifier hash
+	uint256 internal immutable _worldcoinExternalNullifier;
 
 	/// @dev The World ID group ID (always 1)
-	uint256 internal immutable _groupId = 1;
+	uint256 internal immutable _worldcoinGroupId = 1;
 
-	/// @dev Whether an address has a verified nullifier hash. Used to guarantee that an address is a verified human
-	mapping(address => uint256) internal _addressToNullifierHash;
+	/// @dev Whether an address has a verified worldcoin nullifier hash. Used to guarantee that an address is a verified human
+	mapping(address => uint256) internal _addressToWorldcoinNullifierHash;
+
+    /// @dev Whether an address has a verified kyc id
+    mapping(address => uint256) internal _addressToKycId;
 
 	/// @param worldId_ The WorldID instance that will verify the proofs
 	/// @param appId The World ID app ID
@@ -52,7 +58,7 @@ contract LeP2PEscrow is AccessControl {
         require(bytes(appId).length > 0, "App ID cannot be empty");
         require(bytes(actionId).length > 0, "Action ID cannot be empty");
         require(address(token_) != address(0), "Token address cannot be 0");
-		_externalNullifier = abi.encodePacked(abi.encodePacked(appId).hashToField(), actionId).hashToField();
+		_worldcoinExternalNullifier = abi.encodePacked(abi.encodePacked(appId).hashToField(), actionId).hashToField();
 		_worldId = worldId_;
         token = token_;
         _setupRole(ARBITRATOR_ROLE, msg.sender);
@@ -64,7 +70,7 @@ contract LeP2PEscrow is AccessControl {
     * @param fiatToTokenExchangeRate Fiat to token exchange rate
     * @param iban IBAN of the seller
     */
-    function createOrder(uint256 amount, uint256 fiatToTokenExchangeRate, string memory iban) onlyRegistered external {
+    function createOrder(uint256 amount, uint256 fiatToTokenExchangeRate, string memory iban) onlyVerifiedHuman external {
         // Check that the amount to be sold is greater than 0
         require(amount > 0, "Amount must be greater than 0");
         
@@ -73,6 +79,8 @@ contract LeP2PEscrow is AccessControl {
         
         // Check that the IBAN is not empty
         require(bytes(iban).length > 0, "IBAN must not be empty");
+
+        amountCheckKYC(amount);
         
         // Transfer tokens to this contract to hold them
         token.transferFrom(msg.sender, address(this), amount);
@@ -92,7 +100,8 @@ contract LeP2PEscrow is AccessControl {
         nextOrderId++;
     }
     
-    function reserveOrder(uint256 id) onlyRegistered external {
+    function reserveOrder(uint256 id) onlyVerifiedHuman external {
+        amountCheckKYC(amount);
         Order storage order = orders[id];
         require(order.seller != address(0), "Order does not exist");
         require(order.buyer == address(0), "Order already has a buyer");
@@ -100,7 +109,7 @@ contract LeP2PEscrow is AccessControl {
         order.buyer = msg.sender;
     }
     
-    function submitPayment(uint256 id, string memory ipfsHash) onlyRegistered external {
+    function submitPayment(uint256 id, string memory ipfsHash) external {
         Order storage order = orders[id];
         require(order.seller != address(0), "Order does not exist");
         require(msg.sender == order.buyer, "Not the buyer");
@@ -108,7 +117,7 @@ contract LeP2PEscrow is AccessControl {
         order.paymentProof = ipfsHash;
     }
     
-    function confirmOrder(uint256 id) onlyRegistered external {
+    function confirmOrder(uint256 id) external {
         Order storage order = orders[id];
         require(msg.sender == order.seller, "Not the seller");
         require(order.buyer != address(0), "Order has no buyer");
@@ -128,7 +137,7 @@ contract LeP2PEscrow is AccessControl {
         emit OrderCompleted(id, order.buyer, order.paymentProof);
     }
 
-    function cancelOrderUser(uint256 id, string memory reason) onlyRegistered external {
+    function cancelOrderUser(uint256 id, string memory reason) external {
         // Retrieve the order to be cancelled
         Order storage order = orders[id];
 
@@ -153,7 +162,7 @@ contract LeP2PEscrow is AccessControl {
         _cancelOrder(id, reason);
     }
 
-    function cancelOrderArbitrator(uint256 id, string memory reason) onlyRegistered external {
+    function cancelOrderArbitrator(uint256 id, string memory reason) external {
         // Retrieve the order to be cancelled
         Order storage order = orders[id];
 
@@ -185,31 +194,73 @@ contract LeP2PEscrow is AccessControl {
         token.transfer(seller, amount);
     }
 
+    // World ID Verification
+
     /// @param signal An arbitrary input from the user, usually the user's wallet address (check README for further details)
 	/// @param root The root of the Merkle tree (returned by the JS widget).
 	/// @param nullifierHash The nullifier hash for this proof, preventing double signaling (returned by the JS widget).
 	/// @param proof The zero-knowledge proof that demonstrates the claimer is registered with World ID (returned by the JS widget).
 	function verifyAndRegister(address signal, uint256 root, uint256 nullifierHash, uint256[8] calldata proof) public {
-        require(nullifierHash != 0, "Nullifier hash cannot be 0");
+    require(nullifierHash != 0, "Nullifier hash cannot be 0");
 		// First, we make sure this person hasn't done this before
-		if (_addressToNullifierHash[msg.sender] != 0) revert AlreadyRegisteredNullifier();
+		if (_addressToWorldcoinNullifierHash[msg.sender] != 0) revert AlreadyRegisteredNullifier();
 
 		// We now verify the provided proof is valid and the user is verified by World ID
 		_worldId.verifyProof(
 			root,
-			_groupId,
+			_worldcoinGroupId,
 			abi.encodePacked(signal).hashToField(),
 			nullifierHash,
-			_externalNullifier,
+			_worldcoinExternalNullifier,
 			proof
 		);
 
 		// We now record the user has done this, so they can't do it again (proof of uniqueness)
-		_addressToNullifierHash[msg.sender] = nullifierHash;
+		_addressToWorldcoinNullifierHash[msg.sender] = nullifierHash;
 	}
+    
 
-    modifier onlyRegistered() {
-        require(_addressToNullifierHash[msg.sender] != 0, "Address not registered");
+    modifier onlyVerifiedHuman() {
+        require(_addressToWorldcoinNullifierHash[msg.sender] != 0, "Address not registered");
         _;
     }
+
+    function amountCheckKYC(uint256) view external {
+        if(amount > 1000e6) {
+            require(_addressToKycId[msg.sender] != 0, "Address needs to be kycd for amounts greater than 1000");
+        }
+    }
+
+    // Polygon ID Verification
+
+    function _beforeProofSubmit(
+        uint64, /* requestId */
+        uint256[] memory inputs,
+        ICircuitValidator validator
+    ) internal view override {
+        // check that the challenge input of the proof is equal to the msg.sender 
+        address addr = GenesisUtils.int256ToAddress(
+            inputs[validator.getChallengeInputIndex()]
+        );
+        require(
+            _msgSender() == addr,
+            "address in the proof is not a sender address"
+        );
+    }
+
+    function _afterProofSubmit(
+        uint64 requestId,
+        uint256[] memory inputs,
+        ICircuitValidator validator
+    ) internal override {
+        require(
+            requestId == TRANSFER_REQUEST_ID && addressToId[_msgSender()] == 0,
+            "proof can not be submitted more than once"
+        );
+
+        uint256 id = inputs[validator.getChallengeInputIndex()];
+        _addressToKycId[_msgSender()] = id;
+    }
+
+
 }
