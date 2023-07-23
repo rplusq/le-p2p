5c77992e1f23be8b006315b36290c4a6cb4ae4be
Reynaldo Quintero <reynaldoquinterom@gmail.com>
2023-07-23 04:37:06 +0200

    web: partial implementation of Push notifs

--- apps/web/components/AppLayout/AppLayout.tsx
+++ apps/web/components/AppLayout/AppLayout.tsx
@@ -5,13 +5,24 @@ import AppHeader from "./AppHeader";
 import { StyledAppLayout } from "./styles";
 import { useAccount } from "wagmi";
 import { usePathname } from "next/navigation";
+import * as PushAPI from "@pushprotocol/restapi";
+import { useEffect } from "react";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const { address } = useAccount();
   const pathname = usePathname();
 
   const noNav = pathname === "/";
-
+  useEffect(() => {
+    const getNotifications = async () => {
+      const notifications = await PushAPI.user.getFeeds({
+        user: `eip155:80001:${address}`, // user address in CAIP
+        env: "staging" as PushAPI.Env,
+      });
+      console.log(notifications);
+    };
+    getNotifications();
+  }, []);
   return (
     <StyledAppLayout>
       {address && <AppHeader />}
